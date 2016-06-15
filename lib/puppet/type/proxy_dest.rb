9ec5d366ad292c36570798d4f67ade99b3bffa5f
WhatsARanjit <whatsaranjit@gmail.com>
2016-06-15 12:51:06 +0000

    Newparams

--- lib/puppet/type/proxy_dest.rb
+++ lib/puppet/type/proxy_dest.rb
@@ -1,5 +1,5 @@
 Puppet::Type.newtype :proxy_dest, :is_capability => true do
-  param :name, :is_namevar => true
-  param :host
-  param :port
+  newparam :name, :is_namevar => true
+  newparam :host
+  newparam :port
 end
